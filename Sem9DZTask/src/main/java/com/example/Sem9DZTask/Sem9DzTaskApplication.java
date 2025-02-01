6b716ccb1fd1ee0c166b0bd2dbb2042cd49b9147
Evgeniy K <kovalev8915512@gmail.com>
2025-02-01 22:44:54 +0300

    adad

---
+++ Sem9DZTask/src/main/java/com/example/Sem9DZTask/Sem9DzTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sem9DZTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem9DzTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem9DzTaskApplication.class, args);
+	}
+
+}
---
+++ Sem9DZTask/src/test/java/com/example/Sem9DZTask/Sem9DzTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sem9DZTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem9DzTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
