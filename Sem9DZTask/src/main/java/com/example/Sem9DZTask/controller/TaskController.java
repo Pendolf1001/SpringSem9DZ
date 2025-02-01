5fbf9546f2cc5abe1eb9d73618e0aefce9a86bde
Evgeniy K <kovalev8915512@gmail.com>
2025-02-01 22:33:46 +0300

    dobavil tasks

---
+++ Sem9DZTask/src/main/java/com/example/Sem9DZTask/controller/TaskController.java
@@ -0,0 +1,60 @@
+package com.example.Sem9DZTask.controller;
+
+import com.example.Sem5DZ.model.Task;
+import com.example.Sem5DZ.model.TaskStatus;
+import com.example.Sem5DZ.service.TaskService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@AllArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+
+
+
+
+    @GetMapping
+    public List<Task> getAllTask(){
+        return  taskService.getAllTask();
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from  Tasks";
+    }
+
+
+    @PostMapping
+    public void transferMoney(@RequestBody Task task){
+       taskService.createTask(task);
+
+    }
+
+    @GetMapping ("/status/{status}")
+    public List<Task> getTaskByStatus(@PathVariable TaskStatus status){
+        return  taskService.getTaskByStatus(status);
+    }
+
+
+    @PutMapping ("/{id}")
+    public void taskUpdateStatus(@PathVariable Long id, @RequestBody Task task){
+        taskService.taskUpdateStatus(id, task);
+
+
+    }
+
+
+    @DeleteMapping ("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+
+
+    }
+
+
+}
